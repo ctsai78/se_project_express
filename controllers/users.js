7ab9d5a02e89477150c83b27502aa6199e19b9dd
Chang Min Tsai <changt@Changs-MacBook-Air.local>
2023-10-05 18:00:49 -0400

    Project 14

--- controllers/users.js
+++ controllers/users.js
@@ -32,7 +32,9 @@ const getCurrentUser = (req, res) => {
 // PATCH /users/:userId - update user data
 const updateUser = (req, res) => {
   const userId = req._id;
-  const { name, avatarUrl } = req.body;
+  // const { name, avatarUrl } = req.body;
+  const name = req.name;
+  const avatarUrl = req.avatarUrl;
 
   Users.findByIdAndUpdate(
     userId,
